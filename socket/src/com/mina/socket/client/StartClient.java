2a349e52a7bbed512590e0d5600830221def6f40
zhangshaowei <zhangshaowei>
2017-03-27 09:44:09 +0800

    tab to spaces

--- socket/src/com/mina/socket/client/StartClient.java
+++ socket/src/com/mina/socket/client/StartClient.java
@@ -3,8 +3,8 @@ package com.mina.socket.client;
 
 //启动客户端，通过控制台收发信息
 public class StartClient {
-	public static void main(String[] args) {
-		Client client = new Client();//传入ip,端口,可用户名
-		client.start();
-	}
+    public static void main(String[] args) {
+        Client client = new Client();//传入ip,端口,可用户名
+        client.start();
+    }
 }
--- thread/src/com/mina/base/client/MinaTimeClient.java
+++ thread/src/com/mina/base/client/MinaTimeClient.java
@@ -1,34 +1,34 @@
 package com.mina.base.client;
 
-import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
-
 import org.apache.mina.core.future.ConnectFuture;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
 import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
 public class MinaTimeClient {
-	
-	public static void main(String[] args) {
-		
-		NioSocketConnector connector = new NioSocketConnector();
-		connector.getFilterChain().addLast( "logger", new LoggingFilter());
-		connector.getFilterChain().addLast( "codec", new ProtocolCodecFilter( new TextLineCodecFactory( Charset.forName( "UTF-8" ))));
-		
-		connector.setConnectTimeout(30);
-		TimeClientHandler resultHandler = new TimeClientHandler();
-		connector.setHandler(resultHandler);
-		ConnectFuture cf = connector.connect(new InetSocketAddress("localhost",8088));
-		
-		cf.awaitUninterruptibly();
-		cf.getSession().write("hello");
-		cf.getSession().write("quit");
-		cf.getSession().getCloseFuture().awaitUninterruptibly();//
-		connector.dispose();
-		resultHandler.getMessage();
-		
-	}
-	
+
+    public static void main(String[] args) {
+
+        NioSocketConnector connector = new NioSocketConnector();
+        connector.getFilterChain().addLast("logger", new LoggingFilter());
+        connector.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
+
+        connector.setConnectTimeout(30);
+        TimeClientHandler resultHandler = new TimeClientHandler();
+        connector.setHandler(resultHandler);
+        ConnectFuture cf = connector.connect(new InetSocketAddress("localhost", 8088));
+
+        cf.awaitUninterruptibly();
+        cf.getSession().write("hello");
+        cf.getSession().write("quit");
+        cf.getSession().getCloseFuture().awaitUninterruptibly();//
+        connector.dispose();
+        resultHandler.getMessage();
+
+    }
+
 }
